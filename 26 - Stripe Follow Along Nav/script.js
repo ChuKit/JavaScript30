9f6b66851c6fee8a0c5650b75eac7514d0714cf9
Chupong CK Kitisopakul <chupong.kitisopakul@gmail.com>
2018-12-26 23:54:51 +1100

    Finish day 26

---
+++ 26 - Stripe Follow Along Nav/script.js
@@ -0,0 +1,41 @@
+const triggers = document.querySelectorAll('.cool > li');
+const background = document.querySelector('.dropdownBackground');
+const nav = document.querySelector('.top');
+
+function handleEnter() {
+  this.classList.add('trigger-enter');
+  setTimeout(() => {
+    if (this.classList.contains('trigger-enter')) {
+      this.classList.add('trigger-enter-active');
+    }
+  }, 150);
+  //above setTimeout, if we write
+  //setTimeout(function() {this.classList...}, 150)
+  //It won't work coz 'this' for inner fn will refer to window not the element.
+  //However with () => 'this' will work for us. It will refer to the element!
+
+  background.classList.add('open');
+
+  const dropdown = this.querySelector('.dropdown');
+  const dropdownCoords = dropdown.getBoundingClientRect();
+  const navCoords = nav.getBoundingClientRect();
+
+  const coords = {
+    height: dropdownCoords.height,
+    width: dropdownCoords.width,
+    top: dropdownCoords.top - navCoords.top,
+    left: dropdownCoords.left - navCoords.left
+  };
+
+  background.style.setProperty('width', `${coords.width}px`);
+  background.style.setProperty('height', `${coords.height}px`);
+  background.style.setProperty('transform', `translate(${coords.left}px, ${coords.top}px)`);
+}
+
+function handleLeave() {
+  this.classList.remove('trigger-enter', 'trigger-enter-active');
+  background.classList.remove('open');
+}
+
+triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
+triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave));
